4374447a3a13aa1f976927175d02f315ef598a1c
SinkLineP <popovartem505@gmail.com>
2019-12-21 20:55:08 +0200

    create migrate "create_product"

---
+++ db/migrate/20191221182516_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+  	create_table :products do |t|
+    
+    	t.string :title #наименование
+    	t.text :description #размеры
+    	t.decimal :price #стоимость
+    	t.decimal :size
+    	t.boolean	:is_spicy
+    	t.boolean	:is_veg
+    	t.boolean :is_best_offer
+    	t.string :path_to_photo
+
+  		t.timestamps
+  	end
+  end
+end
