3e5176344fde4500bff950a44ec14a4aac49b790
SinkLineP <popovartem505@gmail.com>
2019-12-21 21:09:27 +0200

    lesson - no finished - 1.25.44

--- db/migrate/20191221182516_create_products.rb
+++ db/migrate/20191221182516_create_products.rb
@@ -5,7 +5,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     	t.string :title #наименование
     	t.text :description #размеры
     	t.decimal :price #стоимость
-    	t.decimal :size
+    	t.decimal :size #
     	t.boolean	:is_spicy
     	t.boolean	:is_veg
     	t.boolean :is_best_offer
