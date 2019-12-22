12a2410357a206d3fbed881124656be822898510
SinkLineP <popovartem505@gmail.com>
2019-12-22 16:24:06 +0200

    create migration for Product

--- db/migrate/20191221182516_create_products.rb
+++ db/migrate/20191221182516_create_products.rb
@@ -1,17 +1,21 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
+	
   def change
   	create_table :products do |t|
     
     	t.string :title #наименование
-    	t.text :description #размеры
+    	t.text :description #описание
     	t.decimal :price #стоимость
-    	t.decimal :size #
-    	t.boolean	:is_spicy
-    	t.boolean	:is_veg
-    	t.boolean :is_best_offer
-    	t.string :path_to_photo
+    	t.decimal :size #размеры
+    	t.boolean	:is_spicy #мясная: "true" or "false"
+    	t.boolean	:is_veg #вегетарианская: "true" or "false"
+    	t.boolean :is_best_offer #лучшее предложение
+    	t.string :path_to_photo #путь к фото
 
   		t.timestamps
   	end
+
+  	
+
   end
 end
