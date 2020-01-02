4bdc8d6f7df302c44bcadf204b74197d95ba3d9b
SinkLineP <popovartem505@gmail.com>
2020-01-02 15:07:03 +0200

    update "app.js"

--- public/app.js
+++ public/app.js
@@ -1,17 +1,27 @@
 function something()
 {
-	var x = window.localStorage.getItem('aaa');
-
-	x = x * 1 + 1;
-
-	window.localStorage.setItem('aaa', x);
-
+	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+	x = x * 1 + 1; // x = x + 1
+	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 	alert(x);
 }
 
-function add_to_cart(id)
-{
-	var x = window.localStorage.getItem('product_' + 'id')
-	x = x * 1 + 1
-	window.localStorage.setItem('product_' + 'id', x)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// function add_to_cart(id)
+// {
+// 	var x = window.localStorage.getItem('product_' + 'id')
+// 	x = x * 1 + 1
+// 	window.localStorage.setItem('product_' + 'id', x)
+//  alert(x)
+// }
